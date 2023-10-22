2d9e1c762069eb647a09dcf59b374fdb060ac288
iamabj1996 <ab02.joseph@gmail.com>
2023-10-22 22:58:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,227 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import { abi, address } from './Table';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {
+	Bar,
+	BarChart,
+	CartesianGrid,
+	Legend,
+	Rectangle,
+	Tooltip,
+	XAxis,
+	YAxis,
+} from 'recharts';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const data = [
+	{
+		name: 'Space',
+		scNo: 2,
+		csNo: 3,
+		brNo: 10,
+	},
+	{
+		name: 'Calibration',
+		scNo: 4,
+		csNo: 8,
+		brNo: 10,
+	},
+	{
+		name: 'Capital Planning',
+		scNo: 4,
+		csNo: 8,
+		brNo: 10,
+	},
+];
+
+const App = () => {
+	const [dataForBar, setDataForBar] = useState([]);
+	const [searchValue, setSearchValue] = useState();
+	const [noRecordsFound, setNoRecordsFound] = useState(false);
+	const loadNuvoChain = async () => {
+		const newData = [];
+
+		const web3Provider = new Web3(
+			new Web3.providers.HttpProvider(
+				'https://a8e2-2405-201-37-7894-ac7f-a7dd-4c20-7a30.ngrok-free.app'
+			)
+		);
+
+		const tokenContract = new web3Provider.eth.Contract(abi, address);
+		const spaceTrx = await tokenContract.methods
+			.getComparedResultForReleaseComNameAppName('Thailand', 'Adani', 'Space')
+			.call({ from: '0xb507d99973a645357c8352DBff23963CA4E4A5d3' });
+		console.log('spaceTrx', spaceTrx);
+		newData.push({
+			name: 'Space',
+			scNo: spaceTrx[3].length,
+			csNo: spaceTrx[4].length,
+			brNo: spaceTrx[5].length,
+		});
+
+		const caliberationTrx = await tokenContract.methods
+			.getComparedResultForReleaseComNameAppName(
+				'Thailand',
+				'Adani',
+				'Calibration'
+			)
+			.call({ from: '0xb507d99973a645357c8352DBff23963CA4E4A5d3' });
+
+		newData.push({
+			name: 'Calibration',
+			scNo: caliberationTrx[3].length,
+			csNo: caliberationTrx[4].length,
+			brNo: caliberationTrx[5].length,
+		});
+
+		console.log('newData', newData);
+
+		const capitalPlanningTrx = await tokenContract.methods
+			.getComparedResultForReleaseComNameAppName(
+				'Thailand',
+				'Adani',
+				'Capital Planning'
+			)
+			.call({ from: '0xb507d99973a645357c8352DBff23963CA4E4A5d3' });
+		console.log(' capitalPlanning', capitalPlanningTrx);
+		newData.push({
+			name: 'Capital Planning',
+			scNo: caliberationTrx[3].length,
+			csNo: caliberationTrx[4].length,
+			brNo: caliberationTrx[5].length,
+		});
+
+		const realEstatePlanningTrx = await tokenContract.methods
+			.getComparedResultForReleaseComNameAppName(
+				'Thailand',
+				'Adani',
+				'Real Estate Asset Management'
+			)
+			.call({ from: '0xb507d99973a645357c8352DBff23963CA4E4A5d3' });
+		console.log(' capitalPlanning', realEstatePlanningTrx);
+		newData.push({
+			name: 'Real Estate Manage ',
+			scNo: realEstatePlanningTrx[3].length,
+			csNo: realEstatePlanningTrx[4].length,
+			brNo: realEstatePlanningTrx[5].length,
+		});
+		setDataForBar(newData);
+	};
+
+	useEffect(() => {
+		loadNuvoChain();
+		setSearchValue('Walmart');
+	}, []);
+
+	console.log('dataForBar', dataForBar);
+
+	const handleSubmit = () => {
+		console.log('this ran', searchValue);
+	};
+
+	useEffect(() => {
+		if (searchValue === 'Walmart') {
+			setNoRecordsFound(false);
+		} else {
+			setNoRecordsFound(true);
+		}
+	}, [searchValue]);
+
+	const handleChange = (e) => {
+		console.log('we', e);
+		setSearchValue(e.target.value);
+	};
+
+	return (
+		<>
+			<Navbar expand='lg' className='bg-primary '>
+				<Container>
+					<Navbar.Brand href='#home' className='text-secondary'>
+						NuvoloChain
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav' style={{ marginLeft: '60%' }}>
+						<Nav>
+							<Nav.Link href='#home' className='text-secondary'>
+								COMPANY REPORTS
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<Container className='mt-5 mb-5'>
+				<Row>
+					<Col></Col>
+					<Col sm={4}>
+						<Form className='d-flex'>
+							<Form.Control
+								type='search'
+								placeholder='Search'
+								className='me-2'
+								aria-label='Search'
+								value={searchValue}
+								onChange={handleChange}
+							/>
+							<Button onClick={handleSubmit}>Search</Button>
+						</Form>
+					</Col>
+					<Col></Col>
+				</Row>
+			</Container>
+			<Row>
+				<Col></Col>
+				<Col>
+					{noRecordsFound && (
+						<div style={{ textAlign: 'center' }}>
+							There is no data for this company
+						</div>
+					)}
+					{!noRecordsFound && (
+						<BarChart
+							width={700}
+							height={300}
+							data={dataForBar}
+							margin={{
+								top: 5,
+								right: 30,
+								left: 20,
+								bottom: 5,
+							}}
+						>
+							<CartesianGrid strokeDasharray='3 3' />
+							<XAxis dataKey='name' />
+							<YAxis />
+							<Tooltip />
+							<Legend />
+							<Bar
+								name='No SI changes'
+								dataKey='scNo'
+								fill='#8884d8'
+								activeBar={<Rectangle fill='pink' stroke='blue' />}
+							/>
+							<Bar
+								name='No CS changes'
+								dataKey='csNo'
+								fill='#82ca9d'
+								activeBar={<Rectangle fill='gold' stroke='purple' />}
+							/>
+							<Bar
+								name='No BR changes'
+								dataKey='brNo'
+								fill='#82ca5f'
+								activeBar={<Rectangle fill='blue' stroke='purple' />}
+							/>
+						</BarChart>
+					)}
+				</Col>
+				<Col></Col>
+			</Row>
+		</>
+	);
+};
 
 export default App;
---
+++ src/Table.js
@@ -0,0 +1,172 @@
+const address = '0xb5c7B9078c9D83dE8F0910bf23CB095630b27F62';
+
+const abi = [
+	{
+		inputs: [
+			{ internalType: 'string', name: '_appName', type: 'string' },
+			{ internalType: 'string', name: '_releaseLabel', type: 'string' },
+			{
+				internalType: 'string[]',
+				name: '_scriptIncludeList',
+				type: 'string[]',
+			},
+			{ internalType: 'string[]', name: '_clientScriptList', type: 'string[]' },
+			{
+				internalType: 'string[]',
+				name: '_businessRulesList',
+				type: 'string[]',
+			},
+			{ internalType: 'int256', name: '_totalScriptIncludes', type: 'int256' },
+			{ internalType: 'int256', name: '_totalClientScripts', type: 'int256' },
+			{ internalType: 'int256', name: '_totalBusinessRules', type: 'int256' },
+		],
+		name: 'addApplicationData',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{ internalType: 'string', name: '_appName', type: 'string' },
+			{ internalType: 'string', name: '_releaseLabel', type: 'string' },
+			{ internalType: 'string', name: '_companyName', type: 'string' },
+			{
+				internalType: 'string[]',
+				name: '_scriptIncludeList',
+				type: 'string[]',
+			},
+			{ internalType: 'string[]', name: '_clientScriptList', type: 'string[]' },
+			{
+				internalType: 'string[]',
+				name: '_businessRulesList',
+				type: 'string[]',
+			},
+			{ internalType: 'int256', name: '_totalScriptIncludes', type: 'int256' },
+			{ internalType: 'int256', name: '_totalClientScripts', type: 'int256' },
+			{ internalType: 'int256', name: '_totalBusinessRules', type: 'int256' },
+		],
+		name: 'addCompareResultData',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+		name: 'applications',
+		outputs: [
+			{ internalType: 'string', name: 'appName', type: 'string' },
+			{ internalType: 'string', name: 'releaseLabel', type: 'string' },
+			{ internalType: 'int256', name: 'totalScriptIncludes', type: 'int256' },
+			{ internalType: 'int256', name: 'totalClientScripts', type: 'int256' },
+			{ internalType: 'int256', name: 'totalBusinessRules', type: 'int256' },
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+		name: 'comparedResult',
+		outputs: [
+			{ internalType: 'string', name: 'appName', type: 'string' },
+			{ internalType: 'string', name: 'releaseLabel', type: 'string' },
+			{ internalType: 'string', name: 'companyName', type: 'string' },
+			{ internalType: 'int256', name: 'totalScriptIncludes', type: 'int256' },
+			{ internalType: 'int256', name: 'totalClientScripts', type: 'int256' },
+			{ internalType: 'int256', name: 'totalBusinessRules', type: 'int256' },
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [{ internalType: 'uint256', name: 'index', type: 'uint256' }],
+		name: 'getApplication',
+		outputs: [
+			{
+				components: [
+					{ internalType: 'string', name: 'appName', type: 'string' },
+					{ internalType: 'string', name: 'releaseLabel', type: 'string' },
+					{
+						internalType: 'string[]',
+						name: 'scriptIncludeList',
+						type: 'string[]',
+					},
+					{
+						internalType: 'string[]',
+						name: 'clientScriptList',
+						type: 'string[]',
+					},
+					{
+						internalType: 'string[]',
+						name: 'businessRulesList',
+						type: 'string[]',
+					},
+					{
+						internalType: 'int256',
+						name: 'totalScriptIncludes',
+						type: 'int256',
+					},
+					{
+						internalType: 'int256',
+						name: 'totalClientScripts',
+						type: 'int256',
+					},
+					{
+						internalType: 'int256',
+						name: 'totalBusinessRules',
+						type: 'int256',
+					},
+				],
+				internalType: 'struct ApplicationDataStorage.ApplicationData',
+				name: '',
+				type: 'tuple',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{ internalType: 'string', name: '_releaseLabel', type: 'string' },
+			{ internalType: 'string', name: '_appName', type: 'string' },
+		],
+		name: 'getApplicationByReleaseAndName',
+		outputs: [
+			{ internalType: 'string', name: '', type: 'string' },
+			{ internalType: 'string', name: '', type: 'string' },
+			{ internalType: 'string[]', name: '', type: 'string[]' },
+			{ internalType: 'string[]', name: '', type: 'string[]' },
+			{ internalType: 'string[]', name: '', type: 'string[]' },
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'getApplicationCount',
+		outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{ internalType: 'string', name: '_releaseLabel', type: 'string' },
+			{ internalType: 'string', name: '_companyName', type: 'string' },
+			{ internalType: 'string', name: '_appName', type: 'string' },
+		],
+		name: 'getComparedResultForReleaseComNameAppName',
+		outputs: [
+			{ internalType: 'string', name: '', type: 'string' },
+			{ internalType: 'string', name: '', type: 'string' },
+			{ internalType: 'string', name: '', type: 'string' },
+			{ internalType: 'string[]', name: '', type: 'string[]' },
+			{ internalType: 'string[]', name: '', type: 'string[]' },
+			{ internalType: 'string[]', name: '', type: 'string[]' },
+			{ internalType: 'int256', name: '', type: 'int256' },
+			{ internalType: 'int256', name: '', type: 'int256' },
+			{ internalType: 'int256', name: '', type: 'int256' },
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+];
+module.exports = { address, abi };
